c69933bacf29bc1bc606d2014b1c009b097f424b
Valentin <Alencruserxx@hotmail.fr>
2018-11-19 13:51:02 +0100

    ajout verification mot de passe avant changements infos user

--- resources/views/edit_user.blade.php
+++ resources/views/edit_user.blade.php
@@ -7,11 +7,25 @@
 			<div class="panel panel-default">
 				<form action="/edit/account" method="POST">
 					{{ csrf_field() }}
-					<input type="text" name="name" value="{{$user->name}}" id="name">
-					<input type="email" name="email" value="{{$user->email}}" id="email">
-					<input type="password" name="checkpass" placeholder="Mot de passe actuel" id="checkpass">
-					<input type="password" name="newpass" placeholder="Nouveau mot de passe" id="newpass">
-					<button type="submit">Changer mes informations</button>
+					<div class="form-group">
+						<label for="name">Nom Prénom</label>
+						<input type="text" name="name" class="form-control" value="{{$user->name}}" id="name">
+					</div>
+					<div class="form-group">
+						<label for="email">Mon email</label>
+						<input type="email" class="form-control" name="email" value="{{$user->email}}" id="email">
+					</div>
+					<div class="form-group">
+						<label for="checkpass">Mot de passe actuel</label>
+						<input type="password" class="form-control" name="checkpass" placeholder="Mot de passe actuel" id="checkpass">
+					</div>
+					<div class="form-group">
+						<label for="newpass">Nouveau mot de passe</label>
+						<input type="password" name="newpass" class="form-control" placeholder="Nouveau mot de passe" id="newpass">
+					</div>
+					<div class="form-group">
+						<button type="submit" class='btn btn-info'>Changer mes informations</button>
+					</div>
 				</form>
 			</div>
 		</div>
